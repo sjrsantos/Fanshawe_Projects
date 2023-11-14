ee1f5ba0d7d0c40afcfe5e51ad265cc75f17af06
Silvio Júnior <silvio-sjunior@hotmail.com>
2023-11-14 09:32:37 -0500

    adding some projects

---
+++ JavaScript3/SportsStore/data.js
@@ -0,0 +1,189 @@
+module.exports = function () {
+  return {
+    products: [
+      {
+        id: 1,
+        name: "Kayak",
+        category: "Watersports",
+        description: "A boat for one person",
+        price: 275,
+      },
+      {
+        id: 2,
+        name: "Lifejacket",
+        category: "Watersports",
+        description: "Protective and fashionable",
+        price: 48.95,
+      },
+      {
+        id: 3,
+        name: "Soccer Ball",
+        category: "Soccer",
+        description: "FIFA-approved size and weight",
+        price: 19.5,
+      },
+      {
+        id: 4,
+        name: "Corner Flags",
+        category: "Soccer",
+        description: "Give your playing field a professional touch",
+        price: 34.95,
+      },
+      {
+        id: 5,
+        name: "Stadium",
+        category: "Soccer",
+        description: "Flat-packed 35,000-seat stadium",
+        price: 79500,
+      },
+      {
+        id: 6,
+        name: "Thinking Cap",
+        category: "Chess",
+        description: "Improve brain efficiency by 75%",
+        price: 16,
+      },
+      {
+        id: 7,
+        name: "Unsteady Chair",
+        category: "Chess",
+        description: "Secretly give your opponent a disadvantage",
+        price: 29.95,
+      },
+      {
+        id: 8,
+        name: "Human Chess Board",
+        category: "Chess",
+        description: "A fun game for the family",
+        price: 75,
+      },
+      {
+        id: 9,
+        name: "Bling King",
+        category: "Chess",
+        description: "Gold-plated, diamond-studded King",
+        price: 1200,
+      },
+      {
+        id: 10,
+        name: "Paddle Gloves",
+        category: "Watersports",
+        description:
+          "Ergonomic gloves designed for comfort and grip during long paddling sessions",
+        price: 25,
+      },
+      {
+        id: 11,
+        name: "Waterproof Action Camera",
+        category: "Watersports",
+        description:
+          "Compact and durable camera to capture your aquatic adventures in high definition",
+        price: 120,
+      },
+      {
+        id: 12,
+        name: "Kayak Anchor Kit",
+        category: "Watersports",
+        description:
+          "Lightweight, portable anchor system, perfect for stabilizing your kayak in various water conditions",
+        price: 35,
+      },
+      {
+        id: 13,
+        name: "Portable Soccer Goal",
+        category: "Soccer",
+        description:
+          "Easy-to-assemble, durable soccer goal, suitable for training and casual games",
+        price: 49.99,
+      },
+      {
+        id: 14,
+        name: "Referee Whistle Kit",
+        category: "Soccer",
+        description:
+          "Professional-grade whistles with lanyards and cleaning kit for soccer referees",
+        price: 15,
+      },
+      {
+        id: 15,
+        name: "Soccer Training Cones",
+        category: "Soccer",
+        description:
+          "Set of 20 high-visibility cones, ideal for agility and dribbling drills",
+        price: 22,
+      },
+      {
+        id: 16,
+        name: "Electronic Chess Timer",
+        category: "Chess",
+        description:
+          "Digital timer for chess games, featuring multiple time settings and a sleek design",
+        price: 40,
+      },
+      {
+        id: 17,
+        name: "Travel Chess Set",
+        category: "Chess",
+        description:
+          "Compact, magnetic chess set, perfect for playing on the go",
+        price: 18,
+      },
+      {
+        id: 18,
+        name: "Chess Strategy Book",
+        category: "Chess",
+        description:
+          "Comprehensive guide with strategies and tips for players of all levels",
+        price: 30,
+      },
+      {
+        id: 19,
+        name: "Trekking Backpack",
+        category: "Camping",
+        description:
+          "Durable, spacious backpack designed for long hiking trips",
+        price: 80,
+      },
+      {
+        id: 20,
+        name: "Portable Camping Stove",
+        category: "Camping",
+        description:
+          "Compact and efficient stove, perfect for cooking meals outdoors",
+        price: 45,
+      },
+      {
+        id: 21,
+        name: "Water Filtration Bottle",
+        category: "Camping",
+        description:
+          "Bottle with built-in filter, ensures safe drinking water while hiking",
+        price: 30,
+      },
+      {
+        id: 22,
+        name: "High-Performance Running Shoes",
+        category: "Running",
+        description: "Lightweight and supportive shoes for serious runners",
+        price: 120,
+      },
+      {
+        id: 23,
+        name: "Heart Rate Monitor",
+        category: "Running",
+        description:
+          "Accurate and comfortable heart rate strap for tracking performance",
+        price: 60,
+      },
+      {
+        id: 24,
+        name: "Reflective Running Vest",
+        category: "Running",
+        description:
+          "High-visibility vest for safe running in low light conditions",
+        price: 25,
+      },
+    ],
+    orders: [],
+  };
+};
